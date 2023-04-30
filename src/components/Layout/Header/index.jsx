c5d9b9ae855534d4a596b22adc647d3231240a4e
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-04-30 17:22:37 +0900

    chore: 홈 로고에 Link 추가

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -1,16 +1,19 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import homeLogo from "../../../assets/home_logo.png";
 import "./style.css";
 
 export default function Header() {
   const location = useLocation();
+
   return (
     <header>
       {location.pathname === "/" ? (
         <span className="intro">토킹이 소개</span>
       ) : (
-        <img className="home-logo" src={homeLogo} alt="홈 로고" />
+        <Link to={"/"}>
+          <img className="home-logo" src={homeLogo} alt="홈 로고" />
+        </Link>
       )}
     </header>
   );
