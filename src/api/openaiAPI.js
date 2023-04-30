f1f322e132b97345e1aad4ab19650fededd5f7ba
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-04-30 21:05:36 +0900

    chore: 사용자 채팅 구현

--- src/api/openaiAPI.js
+++ src/api/openaiAPI.js
@@ -6,15 +6,32 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-export const getSystemResponse = async () => {
+export const sendSystemMessage = async () => {
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [
       {
         role: "system",
         content:
-          "You are a Korean elementary student. Your task is to talk about a story with elementary students. Your main objective is to ask a question that assumes particular situation in the story to help elementary students to rewrite a short story. If the user answers your question, you also answer simply and then ask next question. Ask one question at a time and don't say in English. For your first assignment, you are tasked with saying hello to 영희, asking her what book she read recently.",
+          "You are a Korean elementary student. Your task is to talk about a story with elementary students. Your main objective is to ask a question that assumes particular situation in the story to help elementary students to rewrite a short story. If the user answers your question, you also answer simply and then ask next question. Ask one question at a time and don't say in English. For your first assignment, you are tasked with saying hello to a friend, asking her what book she read recently.",
       },
     ],
   });
+
+  return completion.data.choices[0].message;
+};
+
+export const sendMessage = async (content) => {
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: content,
+      },
+    ],
+    frequency_penalty: 0.7,
+  });
+
+  return completion.data.choices[0].message;
 };
--- src/pages/ChatPage/index.jsx
+++ src/pages/ChatPage/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoSend } from "react-icons/io5";
 
 import talkingProfile from "../../assets/talking_profile.png";
@@ -6,21 +6,49 @@ import { colorTheme } from "../../theme";
 import "./style.css";
 
 export default function ChatPage() {
+  // 사용자 답변
+  const [userAnswer, setUserAnswer] = useState("");
+  // 채팅 리스트
+  const [contentList, setContentList] = useState([]);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    if (userAnswer !== "") {
+      // 채팅 리스트에 사용자 답변 추가
+      setContentList((current) => [
+        ...current,
+        { role: "user", text: userAnswer },
+      ]);
+      setUserAnswer(""); // 초기화
+    }
+  };
+
   return (
     <>
       <div className="section--content">
-        <div className="chat chat--talking">
-          <img className="profile--talking" src={talkingProfile} alt="" />
-          <p className="text text--talking">
-            안녕 영희! 요즘에 어떤 책을 읽었어?
-          </p>
-        </div>
-        <div className="chat chat--user">
-          <p className="text text--user">이상한 나라의 앨리스를 읽었어</p>
-        </div>
+        {contentList.map((item, index) =>
+          item.role === "user" ? (
+            <div key={`user${index}`} className="chat chat--user">
+              <p className="text text--user">{item.text}</p>
+            </div>
+          ) : (
+            <div key={`talking${index}`} className="chat chat--talking">
+              <img className="profile--talking" src={talkingProfile} alt="" />
+              <p className="text text--talking">
+                안녕 영희! 요즘에 어떤 책을 읽었어?
+              </p>
+            </div>
+          )
+        )}
       </div>
-      <form className="form--chat">
-        <input className="input--chat" type="text" />
+      <form onSubmit={onSubmit} className="form--chat">
+        <input
+          value={userAnswer}
+          onChange={(event) => setUserAnswer(event.target.value)}
+          className="input--chat"
+          type="text"
+        />
         <button className="send-button">
           <IoSend size={24} color={colorTheme.grey} />
         </button>
