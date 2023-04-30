5b3f95e35beac0c97eefb47bca8d8b47c5b6dba1
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-04-30 15:50:16 +0900

    feat: 홈 화면 추가

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
+import "./style.css";
+
 export default function Home() {
+  function onClickIntro() {
+    // 토킹이 소개 화면 보이기
+  }
+
   return (
     <div>
-      <button>토킹이 소개</button>
+      <header>
+        <span onClick={onClickIntro}>토킹이 소개</span>
+      </header>
+      <main>
+        <img src="logo_nobg_512.png" alt="토킹이 로고" />
+        <button>토킹이와 이야기 시작하기</button>
+      </main>
     </div>
   );
 }
