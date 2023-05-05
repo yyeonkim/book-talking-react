8314a463207a211292234486ecdb61e04835edb2
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-05-05 23:13:58 +0900

    chore: 노란 버튼 컴포넌트 추가

---
+++ src/components/YellowButton/index.jsx
@@ -0,0 +1,5 @@
+import "./style.css";
+
+export default function YellowButton({ text }) {
+  return <button className="yellow-button">{text}</button>;
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import YellowButton from "../../components/YellowButton";
+
 import "./style.css";
 
 export default function Home() {
@@ -9,7 +11,7 @@ export default function Home() {
       <div className="main center">
         <img className="img--home" src="logo_nobg_512.png" alt="토킹이 로고" />
         <Link to={"chat"}>
-          <button className="main-button">토킹이와 이야기 시작하기</button>
+          <YellowButton text="토킹이와 이야기 시작하기" />
         </Link>
       </div>
     </>
