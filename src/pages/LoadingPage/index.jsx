8e99713e282408c6427efcfcc5716ccdb4b0bb8d
yyeonkim <seyeon.dev@gmail.com>
2023-05-05 18:09:05 +0900

    style: 로딩 화면 스타일 수정

--- src/pages/LoadingPage/index.jsx
+++ src/pages/LoadingPage/index.jsx
@@ -6,8 +6,12 @@ export default function LoadingPage() {
     <>
       <div className="main center">
         <img src="logo_no_text.png" alt="토킹이 로고" />
-        <p>즐거운 대화였어!</p>
-        <p>이야기를 만들고 있으니 잠시만 기다려줘.</p>
+        <p>
+          즐거운 대화였어!
+          <br />
+          이야기를 만들고 있으니 잠시만 기다려줘.
+        </p>
+
         <Loader />
       </div>
     </>
