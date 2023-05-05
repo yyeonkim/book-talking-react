27de522cb9ffed91c18a55377eaf40627bafd293
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-05-05 19:44:04 +0900

    style: className 지정

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <div className="main center">
-        <img src="logo_nobg_512.png" alt="토킹이 로고" />
+        <img className="img--home" src="logo_nobg_512.png" alt="토킹이 로고" />
         <Link to={"chat"}>
           <button className="main-button">토킹이와 이야기 시작하기</button>
         </Link>
--- src/pages/LoadingPage/index.jsx
+++ src/pages/LoadingPage/index.jsx
@@ -14,8 +14,12 @@ export default function LoadingPage({ chatList }) {
   return (
     <>
       <div className="main center">
-        <img src="logo_no_text.png" alt="토킹이 로고" />
-        <p>
+        <img
+          className="img--loading"
+          src="logo_no_text.png"
+          alt="토킹이 로고"
+        />
+        <p className="p--loading">
           즐거운 대화였어!
           <br />
           이야기를 만들고 있으니 잠시만 기다려줘.
