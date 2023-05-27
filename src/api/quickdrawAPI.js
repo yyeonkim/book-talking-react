3e55a991bce90ec930f8f5a45b2701363cc69087
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-05-27 22:07:43 +0900

    feat: 서버에 키워드 리스트 보내기

--- src/api/quickdrawAPI.js
+++ src/api/quickdrawAPI.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export const getImageByKeyword = async (keywordList) => {
-  return await axios.post("http://127.0.0.1:8080/keyword-image", keywordList);
+  return await axios.post("http://127.0.0.1:8080/api/keyword-image", {
+    keywords: keywordList,
+  });
 };
