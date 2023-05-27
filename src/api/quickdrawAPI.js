7233bbd117a52d740ffcaa6a50af191535c290ea
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-05-28 00:57:43 +0900

    feat: quickdraw에서 image 좌표 받아오기

--- src/api/quickdrawAPI.js
+++ src/api/quickdrawAPI.js
@@ -1,7 +1,23 @@
 import axios from "axios";
 
 export const getImageByKeyword = async (keywordList) => {
-  return await axios.post("http://127.0.0.1:8080/api/keyword-image", {
-    keywords: keywordList,
-  });
+  try {
+    const response = await axios.post(
+      "http://127.0.0.1:8080/api/keyword-image",
+      {
+        keywords: keywordList,
+      }
+    );
+    if (response.status === 200) {
+      return response;
+    } else if (response.status === 404) {
+      console.log("try again");
+      // 새로운 키워드 받아오기
+      return;
+    } else {
+      throw Error();
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
--- src/pages/DrawingPage/index.jsx
+++ src/pages/DrawingPage/index.jsx
@@ -45,17 +45,7 @@ function DrawingPage() {
   //   state: { story, title },
   // } = useLocation();
 
-  useEffect(() => {
-    // 동화에서 키워드 추출
-    getKeywordList({
-      story:
-        "영희는 최근에 '이상한 나라의 앨리스'를 읽고, 이상한 나라로 여행을 가고 싶어졌어요. 그녀는 체셔 고양이를 만나기 위해 여행을 하게 되었고, 준비물로는 체셔 고양이가 좋아하는 마들렌 케이크를 가져갔어요. 이제 영희와 체셔 고양이의 모험이 시작됐습니다! 체셔 고양이와 함께 이동하면서, 영희는 토끼도 만나기로 했어요. 그녀는 토끼를 쫓으며 무슨 일이 일어날까 기대하기도 하지만 길을 잃거나 무서워하기도 해요. 하지만 언제든지 엄마나 아빠에게 도움을 받으며 안전하게 여행할 수 있다면, 영희와 체셔 고양시의 모험은 계속 됩니다!",
-    }).then(async (res) => {
-      const keywordList = res.data.content.slice(9, -1).split(",");
-      await getImageByKeyword(keywordList);
-    });
-  }, []);
-
+  const [aiDrawing, setAiDrawing] = useState([]);
   const canvasRef = useRef(null);
   const parentOfCanvasRef = useRef(null); // 캔버스 부모 요소
   const [canvas, setCanvas] = useState(null);
@@ -68,6 +58,18 @@ function DrawingPage() {
   const [action, setAction] = useState(Action.Pencil); // 그리기 액션 (pencil, eraser, paint)
   const [drawing, setDrawing] = useState(false); // 그리는 중이면 true 아니면 false
   const [erasing, setErasing] = useState(false); // 지우는 중이면 true 아니면 false
+
+  useEffect(() => {
+    // 동화에서 키워드 추출
+    getKeywordList({
+      story:
+        "영희는 최근에 '이상한 나라의 앨리스'를 읽고, 이상한 나라로 여행을 가고 싶어졌어요. 그녀는 체셔 고양이를 만나기 위해 여행을 하게 되었고, 준비물로는 체셔 고양이가 좋아하는 마들렌 케이크를 가져갔어요. 이제 영희와 체셔 고양이의 모험이 시작됐습니다! 체셔 고양이와 함께 이동하면서, 영희는 토끼도 만나기로 했어요. 그녀는 토끼를 쫓으며 무슨 일이 일어날까 기대하기도 하지만 길을 잃거나 무서워하기도 해요. 하지만 언제든지 엄마나 아빠에게 도움을 받으며 안전하게 여행할 수 있다면, 영희와 체셔 고양시의 모험은 계속 됩니다!",
+    }).then(async (res) => {
+      const keywordList = res.data.content.slice(9, -1).split(",");
+      const response = await getImageByKeyword(keywordList);
+      setAiDrawing(response.data._drawing_data.image);
+    });
+  }, []);
 
   useEffect(() => {
     const canvas = canvasRef.current;
