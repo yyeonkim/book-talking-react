35e47b87fe0c16e0c7c9d3deaf003df8df3bcad5
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-05-29 22:18:31 +0900

    chore: assets에서 이미지 가져오기

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,16 +3,13 @@ import { Link } from "react-router-dom";
 
 import "./style.css";
 import YellowButton from "../../components/YellowButton";
+import homeLogo from "../../assets/logo_nobg_512.png";
 
 export default function Home() {
   return (
     <>
       <div className="Home center">
-        <img
-          className="Home__image"
-          src="logo_nobg_512.png"
-          alt="토킹이 로고"
-        />
+        <img className="Home__image" src={homeLogo} alt="토킹이 로고" />
         <Link to={"chat"}>
           <YellowButton text="토킹이와 이야기 시작하기" />
         </Link>
