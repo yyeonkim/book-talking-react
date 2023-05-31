45ea118c87cf6b14b7b709de925401f88b580d9e
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-05-31 18:09:22 +0900

    feat: 터치 시 스크롤 이벤트 막기

--- src/pages/DrawingPage/index.jsx
+++ src/pages/DrawingPage/index.jsx
@@ -1,8 +1,24 @@
+import { useEffect } from "react";
+
 import "./style.css";
 import Canvas from "../../components/Canvas";
 import AiCanvas from "../../components/AiCanvas";
 
 function DrawingPage() {
+  useEffect(() => {
+    const preventPageRefresh = (event) => {
+      event.preventDefault();
+    };
+
+    document.addEventListener("touchmove", preventPageRefresh, {
+      passive: false,
+    });
+
+    return () => {
+      document.removeEventListener("touchmove", preventPageRefresh);
+    };
+  }, []);
+
   return (
     <div className="DrawingPage">
       <AiCanvas />
