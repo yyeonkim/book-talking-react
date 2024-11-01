dded60c6a3482cd086671ed54b27af786b9dabe0
Yongyeon Kim <seyeon.dev@gmail.com>
2024-11-01 09:57:49 +0900

    chore: 로컬 환경으로 변경

--- src/api/chatAPI.js
+++ src/api/chatAPI.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
-const baseUrl = "http://54.165.187.0:5000";
+// const baseUrl = "http://54.165.187.0:5000";
 
 export const startChat = async (systemMessage) => {
-  return await axios.post(`${baseUrl}/api/chat/start`, systemMessage);
+  return await axios.post(`/api/chat/start`, systemMessage);
 };
 
 export const sendMessage = async (chatList) => {
-  return await axios.post(`${baseUrl}/api/chat/send`, chatList);
+  return await axios.post(`/api/chat/send`, chatList);
 };
 
 export const summarizeChat = async (chatList) => {
-  return await axios.post(`${baseUrl}/api/chat/summarize`, chatList);
+  return await axios.post(`/api/chat/summarize`, chatList);
 };
 
 export const getKeywordList = async (chatList) => {
-  return await axios.post(`${baseUrl}/api/chat/keyword`, chatList);
+  return await axios.post(`/api/chat/keyword`, chatList);
 };
 
 export const translateKeyword = async (keyword) => {
-  return await axios.get(`${baseUrl}/api/chat/translate`, {
+  return await axios.get(`/api/chat/translate`, {
     params: { keyword },
   });
 };
--- src/api/quickdrawAPI.js
+++ src/api/quickdrawAPI.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
+// const baseUrl = "http://35.172.220.99:5000";
+
 export const getImageByKeyword = async (keywordList) => {
   try {
-    const response = await axios.post(
-      "http://35.172.220.99:5000/api/keyword-image",
-      {
-        keywords: keywordList,
-      }
-    );
+    const response = await axios.post("/api/keyword-image", {
+      keywords: keywordList,
+    });
     if (response.status === 200) {
       return { message: "success", data: response.data };
     }
